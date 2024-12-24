0e09caf658223c91ca436709994e88dcfcab8f80
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-12-24 12:38:42 +0700

    copy RetrofitForecaster project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.md_lab09__activitycycle_n_statemanagement"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.md_lab09__activitycycle_n_statemanagement"
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.glide)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/md_lab09__activitycycle_n_statemanagement/MainActivity.kt
+++ app/src/main/java/com/example/md_lab09__activitycycle_n_statemanagement/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        val rView: RecyclerView = findViewById(R.id.rView)
+        val adapter = Adapter(DiffCallback())
+
+        setSupportActionBar(toolbar)
+        rView.adapter = adapter
+        rView.layoutManager = LinearLayoutManager(this)
+
+        val mService: RetrofitServices = Common.retrofitService
+        val lat = 54.2021736
+        val lon = 30.2964015
+        val appId = "your api key"
+
+        mService.getWeatherList(lat, lon, appId).enqueue(object : Callback<WeatherWrapper> {
+            override fun onResponse(call: Call<WeatherWrapper>, response: Response<WeatherWrapper>) {
+                if (response.isSuccessful) {
+                    val forecast = response.body()
+                    adapter.submitList(forecast!!.list)
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) { }
+        })
     }
 }
